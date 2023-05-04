e02695fde483c4c52bd45d9498c844f91aa59d86
Elmagnitiko <zhenyazhurov@gmail.com>
2023-05-04 16:08:39 +0300

    CommittedTrainingGIT

---
+++ TrainingGIT/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TrainingGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!");
+        }
+    }
+}

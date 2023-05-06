25b058548ec9fa85e62961042460b69a54940a96
Elmagnitiko <zhenyazhurov@gmail.com>
2023-05-06 12:44:48 +0300

    I noticed that you missed a fourth line in ur awesome code! r welcome bruh

--- TrainingGIT/Program.cs
+++ TrainingGIT/Program.cs
@@ -8,6 +8,7 @@ namespace TrainingGIT
             Console.WriteLine("Hello, GitHub!");
             Console.WriteLine("Just another one line.");
             Console.WriteLine("And a third line!");
+            Console.WriteLine("What if i add another one line?");
         }
     }
 }
